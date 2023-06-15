436541257a5b9532fe3aa3fb5f1b33f94766d6e6
Ricardo Coimbra - CURA <coimbra.rico@gmail.com>
2023-06-15 11:14:02 -0400

    Componente Topo

---
+++ resources/js/components/Topo.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" :href="url">
+                    {{ titulo }}
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav me-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ms-auto">
+                        <slot></slot>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</template>
+
+<script>
+export default {
+    name: 'Topo',
+    props: ['titulo', 'url']
+}
+</script>
+
+<style>
+
+</style>
