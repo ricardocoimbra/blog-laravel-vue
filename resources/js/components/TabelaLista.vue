84f9f498e14788a617dede17f32dc060f09755be
Ricardo Coimbra - RC <coimbra.rico@gmail.com>
2023-06-26 23:28:22 -0400

    Fim da aula 22 - Ordenação da lista

--- resources/js/components/TabelaLista.vue
+++ resources/js/components/TabelaLista.vue
@@ -12,7 +12,7 @@
         <table class="table table-bordered table-hover">
             <thead>
             <tr>
-                <th v-for="titulo in titulos">{{ titulo }}</th>
+                <th style="cursor:pointer" @click="ordenaColuna(index)" v-for="(titulo, index) in titulos">{{ titulo }}</th>
 
                 <th v-if="detalhes || editar || deletar">Ação</th>
             </tr>
@@ -49,19 +49,48 @@
 <script>
 export default {
     name: 'TabelaLista',
-    props: ['titulos', 'itens', 'criar', 'detalhes', 'editar', 'deletar', 'token'],
+    props: ['titulos', 'itens', 'ordem', 'ordemcol', 'criar', 'detalhes', 'editar', 'deletar', 'token'],
     data () {
         return {
-            buscar: ''
+            buscar: '',
+            ordemAux: this.ordem || "asc",
+            ordemColAux: this.ordemcol || 0
         }
     },
     methods: {
-        executaForm (index) {
+        executaForm: function (index) {
             document.getElementById(index).submit();
+        },
+        ordenaColuna: function (coluna) {
+            this.ordemColAux = coluna;
+            if(this.ordemAux.toLowerCase() === "asc") {
+                this.ordemAux = "desc"
+            } else {
+                this.ordemAux = "asc";
+            }
         }
     },
     computed: {
-        lista () {
+        lista: function () {
+            let ordem = this.ordemAux || "asc";
+            let ordemCol = this.ordemColAux || 0;
+            ordem = ordem.toLowerCase();
+            ordemCol = parseInt(ordemCol);
+
+            if (ordem === "asc") {
+                this.itens.sort((a, b) => {
+                    if (a[ordemCol] > b[ordemCol]) {return 1;}
+                    if (a[ordemCol] < b[ordemCol]) {return -1;}
+                    return 0;
+                });
+            } else {
+                this.itens.sort((a, b) => {
+                    if (a[ordemCol] < b[ordemCol]) {return 1;}
+                    if (a[ordemCol] > b[ordemCol]) {return -1;}
+                    return 0;
+                });
+            }
+
             return this.itens.filter(res => {
                 for (let k = 0; k < res.length; k++) {
                     if ((res[k] + "").toLowerCase().indexOf(this.buscar.toLowerCase()) >= 0) {
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -10,6 +10,7 @@
                 <tabela-lista
                     :titulos="['#', 'TÍtulo', 'Descrição', 'Autor', 'Data']"
                     :itens="[[1, 'PHP OO', 'Curso de PHP OO', 'Ricardo Coimbra', '10/10/2022'], [2, 'VueJS', 'Curso de VueJS', 'Ricardo Coimbra', '10/11/2022']]"
+                    ordem="desc" ordemcol="1"
                     criar="{{route('artigos.create')}}" detalhes="#detalhes" editar="#editar" deletar="#deletar" token="#token"
                 ></tabela-lista>
 
